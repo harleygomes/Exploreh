c39fff66ec094c790677a09fecccdcadbbf46b1f
Harley Gomes <harley.gomes@gmail.com>
2017-06-10 00:25:35 -0300

    1st commit

---
+++ Exploreh.Model/Cliente/ClienteTelefoneModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exploreh.Database;
+using Exploreh.Model.Cidade;
+using Exploreh.Model.Estado;
+
+namespace Exploreh.Model.Cliente
+{
+    public class ClienteTelefoneModel
+    {
+        public int Id { get; set; }
+        public string Ddd { get; set; }
+        public string Telefone { get; set; }
+        public System.DateTime DataCadastro { get; set; }
+        public Nullable<System.DateTime> DataAlteracao { get; set; }
+        public Nullable<bool> Ativo { get; set; }
+        public int ClienteId { get; set; }
+
+        public virtual ClienteModel Cliente { get; set; }
+
+        public static implicit operator ClienteTelefoneModel(Database.ClienteTelefone ct)
+        {
+            return new ClienteTelefoneModel
+            {
+                Id = ct.Id,
+                ClienteId = ct.ClienteId,
+                Ddd = ct.Ddd,
+                Telefone = ct.Telefone,
+                DataCadastro = ct.DataCadastro,
+                DataAlteracao = ct.DataAlteracao,
+                Ativo = ct.Ativo
+            };
+        }
+        public static implicit operator ClienteTelefone(ClienteTelefoneModel ct)
+        {
+            return new ClienteTelefone
+            {
+                Id = ct.Id,
+                ClienteId = ct.ClienteId,
+                Ddd = ct.Ddd,
+                Telefone = ct.Telefone,
+                DataCadastro = ct.DataCadastro,
+                DataAlteracao = ct.DataAlteracao,
+                Ativo = ct.Ativo
+            };
+        }
+    }
+}
---
+++ Exploreh.Repository/Base/BaseRepository.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using Exploreh.Database;
+
+namespace Exploreh.Repository.Base
+{
+    /// <summary>
+    /// Esta classe tenta otimizar o esforço quanto a criação de CRUD´s recorrentes em nossos projetos
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class BaseRepository<T> where T : class
+    {
+        private ExplorehEntities _db;
+        private IDbSet<T> entities;
+
+        public BaseRepository()
+        {
+            this._db = new ExplorehEntities();
+        }
+
+        /// <summary>
+        /// Método que retorna uma entidade pelo seu ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public T Get(int id)
+        {
+            return this.Entities.Find(id);
+        }
+
+        /// <summary>
+        /// Método que retorna uma lista "T"
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public List<T> Get()
+        {
+            return this.Entities.ToList();
+        }
+
+        /// <summary>
+        /// Método que adiciona uma nova entidade
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public bool Add(T entity)
+        {
+            try
+            {
+                if (entity == null) return false;
+
+                this.Entities.Add(entity);
+                return this._db.SaveChanges() > 0;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        /// Método que atualiza uma entidade
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public bool Update(T entity)
+        {
+            try
+            {
+                if (entity == null) return false;
+
+                #region Alternativas viáveis
+                /*this._db.Entry(entity).State = EntityState.Modified;*/
+                /*this.Entities.Attach(entity);*/
+                #endregion
+
+                this._db.Set<T>().AddOrUpdate(entity);
+                return this._db.SaveChanges() > 0;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        /// Método que deleta uma entidade
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public bool Delete(T entity)
+        {
+            try
+            {
+                if (entity == null) return false;
+
+                this._db.Entry(entity).State = EntityState.Deleted;
+                return this._db.SaveChanges() > 0;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual IQueryable<T> Table
+        {
+            get
+            {
+                return this.entities;
+            }
+        }
+
+        /// <summary>
+        /// Propriedade que identifica a entidade corrente
+        /// </summary>
+        private IDbSet<T> Entities
+        {
+            get
+            {
+                if (entities == null)
+                {
+                    entities = _db.Set<T>();
+                }
+                return entities;
+            }
+        }
+
+    }
+}
+
