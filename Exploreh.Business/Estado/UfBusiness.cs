dbb845097dd320e10d5b48d948490447afac352e
EDER\Eder Costa <eder@ponte21.com.br>
2017-07-24 02:48:50 -0300

    Desenvolvendo cadastro Estado / Cidade por modal - incompleto

---
+++ Exploreh.Business/Estado/UfBusiness.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exploreh.Model.Uf;
+using Exploreh.Model.UnidadeFederacao;
+using Exploreh.Repository.Repository;
+
+namespace Exploreh.Business.Estado
+{
+    public class UfBusiness
+    {
+        private readonly GenericRepository<Database.Estado> _rep;
+
+
+        public UfBusiness()
+        {
+            this._rep = new GenericRepository<Database.Estado>();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<UfModel> Get()
+        {
+            return _rep.Get().ToList().ConvertAll<UfModel>(x => x);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public UfModel Get(int id)
+        {
+            return _rep.Get(id);
+        }
+
+        public bool Add(UfModel model)
+        {
+            return _rep.Add(model);
+        }
+
+    }
+}
---
+++ Exploreh.Model/Uf/UfModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exploreh.Database;
+using Exploreh.Model.Cidade;
+
+namespace Exploreh.Model.Uf
+{
+    public class UfModel
+    {
+        public int Id { get; set; }
+        public string Sigla { get; set; }
+        public string Nome { get; set; }
+        
+        public virtual ICollection<CidadeModel> Cidade { get; set; }
+
+        public static implicit operator UfModel(Database.Estado uf)
+        {
+            return new UfModel
+            {
+                Id = uf.Id,
+                Sigla = uf.Sigla,
+                Nome = uf.Nome
+            };
+        }
+        public static implicit operator Database.Estado(UfModel uf)
+        {
+            return new Database.Estado
+            {
+                Id = uf.Id,
+                Sigla = uf.Sigla,
+                Nome = uf.Nome
+            };
+        }
+    }
+}
