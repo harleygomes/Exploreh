f15ab2f3a9b2f7d2b3c56c4c442348faa8fca8fb
EDER\Eder Costa <eder@ponte21.com.br>
2017-09-20 23:25:03 -0300

    corrigindo popup de detalhes na listagem de cliente

--- Exploreh.Web/Scripts/ClienteAjaxCall.js
+++ Exploreh.Web/Scripts/ClienteAjaxCall.js
@@ -67,47 +67,69 @@ function ExcluirConfirmar(model, url) {
 
 function HtmlBuildEditar(data) {
 
-    return [
+    return $(
         '<div class="panel-body" id="dynamicModal">' +
         '<div class="input-group input-fields">' +
         '<span class="input-group-addon">Nome</span>' +
-        '<input disabled id="Nome" type="text" class="form-control" name="Nome" value="' + data.Nome + '">' +
+        '<input disabled id="Nome" type="text" class="form-control" name="Nome" value="' +
+        data.Nome +
+        '">' +
         '</div>' +
         '<div class="input-group input-fields">' +
         '<span class="input-group-addon">E-mail</span>' +
-        '<input disabled id="Email" type="text" class="form-control" name="Email" value="' + data.Email + '">' +
+        '<input disabled id="Email" type="text" class="form-control" name="Email" value="' +
+        data.Email +
+        '">' +
         '</div>' +
         '<div class="input-group input-fields">' +
         '<span class="input-group-addon">Ativo</span>' +
-        '<div style="margin-left: 10px"> <input disabled type="checkbox" name="Ativo"' + (data.Ativo === true ? 'checked="checked"' : '') + '"></div>' +
+        '<div style="margin-left: 10px"> <input disabled type="checkbox" name="Ativo"' +
+        (data.Ativo === true ? 'checked="checked"' : '') +
+        '"></div>' +
         '</div>' +
         '<div class="input-group input-fields">' +
         '<span class="input-group-addon">Documento</span>' +
-        '<input disabled id="Documento" type="text" class="form-control" name="Documento" value="' + data.Documento + '">' +
+        '<input disabled id="Documento" type="text" class="form-control" name="Documento" value="' +
+        data.Documento +
+        '">' +
         '</div>' +
         '<div class="input-group input-fields">' +
-        '<span class="input-group-addon">' + (data.ClienteTelefone[0].TipoTelefone === "C" ? 'Celular ' : 'Fixo') + '</span>' +
-        '<input disabled id="ClienteTelefone[1]_Ddd" type="text" class="form-control" name="ClienteTelefone[1].Ddd" value="' + data.ClienteTelefone[1].Ddd + '">' +
+        '<span class="input-group-addon">' +
+        (data.ClienteTelefone[0].TipoTelefone === "C" ? 'Celular ' : 'Fixo') +
+        '</span>' +
+        '<input disabled id="ClienteTelefone[1]_Ddd" type="text" class="form-control" name="ClienteTelefone[1].Ddd" value="' +
+        data.ClienteTelefone[0].Ddd +
+        '">' +
         '</div>' +
         '<div class="input-group input-fields">' +
         '<span class="input-group-addon">Telefone</span>' +
-        '<input disabled id="ClienteTelefone[1]_Telefone" type="text" class="form-control" name="ClienteTelefone[1].Telefone" value="' + data.ClienteTelefone[1].Telefone + '">' +
+        '<input disabled id="ClienteTelefone[1]_Telefone" type="text" class="form-control" name="ClienteTelefone[1].Telefone" value="' +
+        data.ClienteTelefone[0].Telefone +
+        '">' +
         '</div>' +
         '<div class="input-group input-fields">' +
         '<span class="input-group-addon">Logradouro</span>' +
-        '<input disabled id="ClienteEndereco[0]_Logradouro" type="text" class="form-control" name="ClienteEndereco[0].Logradouro" value="' + data.ClienteEndereco[0].Logradouro + '">' +
+        '<input disabled id="ClienteEndereco[0]_Logradouro" type="text" class="form-control" name="ClienteEndereco[0].Logradouro" value="' +
+        data.ClienteEndereco[0].Logradouro +
+        '">' +
         '</div>' +
         '<div class="input-group input-fields">' +
         '<span class="input-group-addon">Logradouro</span>' +
-        '<input disabled id="ClienteEndereco[0]_Numero" type="text" class="form-control" name="ClienteEndereco[0].Numero" value="' + data.ClienteEndereco[0].Numero + '">' +
+        '<input disabled id="ClienteEndereco[0]_Numero" type="text" class="form-control" name="ClienteEndereco[0].Numero" value="' +
+        data.ClienteEndereco[0].Numero +
+        '">' +
         '</div>' +
         '<div class="input-group input-fields">' +
         '<span class="input-group-addon">Complemento</span>' +
-        '<input disabled id="ClienteEndereco[0]_Complemento" type="text" class="form-control" name="ClienteEndereco[0].Complemento" value="' + data.ClienteEndereco[0].Complemento + '">' +
+        '<input disabled id="ClienteEndereco[0]_Complemento" type="text" class="form-control" name="ClienteEndereco[0].Complemento" value="' +
+        data.ClienteEndereco[0].Complemento +
+        '">' +
         '</div>' +
         '<div class="input-group input-fields">' +
         '<span class="input-group-addon">Bairro</span>' +
-        '<input disabled id="ClienteEndereco[0]_Bairro" type="text" class="form-control" name="ClienteEndereco[0].Bairro" value="' + data.ClienteEndereco[0].Bairro + '">' +
+        '<input disabled id="ClienteEndereco[0]_Bairro" type="text" class="form-control" name="ClienteEndereco[0].Bairro" value="' +
+        data.ClienteEndereco[0].Bairro +
+        '">' +
         '</div>' +
         '<div class="input-group input-fields">' +
         //'<span class="input-group-addon">Nome do Contato</span>' +
@@ -125,8 +147,8 @@ function HtmlBuildEditar(data) {
         //'<span class="input-group-addon">Estado</span>' +
         //'<input disabled id="ClienteEndereco[0]_Estado" type="text" class="form-control" name="ClienteEndereco[0].Estado" value="' + data.Estado[0].Nome + '">' +
         //'</div>' +
-        '</div>'
-    ];
+        '</div>');
+
 
 }
 
